8b533b8a8592639d4bdd1dc04f6818f6b308b838
Sebadev7 <sebadev7@gmail.com>
2024-09-18 20:56:30 -0300

    cambiando valor mensaje de alerta

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,7 @@ function actualizarListaGastos() {
        listaElementos.innerHTML = htmlLista;
        totalElementos.innerHTML = totalGastos.toFixed(2);
 if (totalGastos > 150){
-    alert("Cuidado estas gastando mas de 150 Pesos");
+    alert("Cuidado estas gastando mas de 500000 Pesos");
 }
 
        limpiar();
